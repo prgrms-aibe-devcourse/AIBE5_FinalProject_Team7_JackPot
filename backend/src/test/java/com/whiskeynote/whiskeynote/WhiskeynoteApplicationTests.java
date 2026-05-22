a887a0b450178750f3915d084d65322294ad5cf5
최준열 <snew4802@gmail.com>
2026-05-22 17:42:20 +0900

    [Feat] #0 - backend 폴더 정상 등록 (submodule 오류 수정)

---
+++ backend/src/test/java/com/whiskeynote/whiskeynote/WhiskeynoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whiskeynote.whiskeynote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskeynoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
