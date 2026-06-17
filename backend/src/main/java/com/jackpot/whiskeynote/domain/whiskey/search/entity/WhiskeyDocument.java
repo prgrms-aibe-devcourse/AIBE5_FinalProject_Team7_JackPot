09552144959e2b1f0e425144abbb7e956e477e66
김민국 <mapletrue01@naver.com>
2026-06-17 10:14:04 +0900

    Feat/be:#382 : 엘라스틱 서치 ngram 추가

--- backend/src/main/java/com/jackpot/whiskeynote/domain/whiskey/search/entity/WhiskeyDocument.java
+++ backend/src/main/java/com/jackpot/whiskeynote/domain/whiskey/search/entity/WhiskeyDocument.java
@@ -27,6 +27,13 @@ public class WhiskeyDocument {
     // 별칭은 여러 개가 있을 수 있으므로 리스트로 저장. 검색 시에도 텍스트 분석기로 처리하여 유연한 검색이 가능하도록 설정.
     @Field(type = FieldType.Text)
     private List<String> aliases;
+    // 검색 시 name과 aliases를 함께 분석하여 검색할 수 있도록 별도의 필드로 설정. 검색어가 name이나 aliases 중 하나와 일치하면 검색 결과에 포함되도록 설정.
+    @Field(
+            type = FieldType.Text,
+            analyzer = "whiskey_index_analyzer",
+            searchAnalyzer = "whiskey_search_analyzer"
+    )
+    private String searchText;
     // Enum -> Keyword로 저장하여 정확한 일치 검색이 가능하도록 설정
     @Field(type = FieldType.Keyword)
     private WhiskeyType type;
